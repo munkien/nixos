af3c734762ff9c760dfe8d6aab913d0f8bfaaae2
munkien <munkien@gmail.com>
2026-01-11 18:52:43 +0100

    WIP

--- mods/system/wifi-gl3.nix
+++ mods/system/wifi-gl3.nix
@@ -3,7 +3,8 @@
   pkgs,
   ...
 }: {
-  sops.secrets.wifi_password = {};
+  sops.secrets.wifi_password_1 = {};
+  sops.secrets.wifi_password_2 = {};
   sops.templates."GL3-5G.nmconnection" = {
     path = "/etc/NetworkManager/system-connections/GL3-5G.nmconnection";
     mode = "0600";
@@ -19,7 +20,31 @@
 
       [wifi-security]
       key-mgmt=wpa-psk
-      psk=${config.sops.secrets.wifi_password.placeholder}
+      psk=${config.sops.secrets.wifi_password_1.placeholder}
+
+      [ipv4]
+      method=auto
+
+      [ipv6]
+      method=auto
+    '';
+  };
+  sops.templates."GL3.nmconnection" = {
+    path = "/etc/NetworkManager/system-connections/GL3.nmconnection";
+    mode = "0600";
+    content = ''
+      [connection]
+      id=GL3
+      type=wifi
+      autoconnect=true
+
+      [wifi]
+      ssid=sild-IoT
+      mode=infrastructure
+
+      [wifi-security]
+      key-mgmt=wpa-psk
+      psk=${config.sops.secrets.wifi_password_2.placeholder}
 
       [ipv4]
       method=auto
