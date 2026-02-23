89c4f227f520aaa57c5ad3d730d9bf174772678d
Anders Munk <55032909+munkien@users.noreply.github.com>
2026-02-23 13:34:41 +0100

    Update caddy.nix

--- mods/quadlets/caddy.nix
+++ mods/quadlets/caddy.nix
@@ -1,1 +1,28 @@
+{ config, ... }:
 
+{
+  users.users.caddy.extraGroups = [ "acme" ];
+
+  services.caddy = {
+    enable = true;
+    logFormat = "format json { time_format iso8601 }";
+    globalConfig = "servers { trusted_proxies static private_ranges }";
+
+    extraConfig = ''
+      (secure_proxy) {
+        encode zstd gzip
+        header Strict-Transport-Security "max-age=31536000;"
+        header X-Content-Type-Options "nosniff"
+        header X-Frame-Options "DENY"
+      }
+    '';
+
+    virtualHosts."munkie.dk" = {
+      useACMEHost = "munkie.dk";
+      extraConfig = ''
+        import secure_proxy
+        respond "Hello World!"
+      '';
+    };
+  };
+}
