7635b549945249c44f31e31328fda086ebecfc30
Anders Munk <55032909+munkien@users.noreply.github.com>
2026-02-23 13:21:08 +0100

    Update default.nix

--- mods/quadlets/default.nix
+++ mods/quadlets/default.nix
@@ -6,40 +6,43 @@
 }:
 
 {
-  # Enable podman
+  # ==========================================
+  # Podman Base
+  # ==========================================
   virtualisation.podman = {
     enable = true;
     dockerSocket.enable = true;
+    defaultNetwork.settings.dns_enabled = false;
+    
     autoPrune = {
       enable = true;
       flags = [ "--all" ];
       dates = "daily";
     };
-    defaultNetwork.settings.dns_enabled = false;
   };
 
+  # Trust the Quadlet bridge network for cross-container & host communication
   networking.firewall.trustedInterfaces = [ "homelab" ];
 
-  virtualisation.quadlet =
-    let
-      inherit (config.virtualisation.quadlet) networks pods;
-    in
-    {
+  # ==========================================
+  # Quadlet Global Settings
+  # ==========================================
+  virtualisation.quadlet = {
+    enable = true;
+    autoEscape = true;
+    
+    autoUpdate = {
       enable = true;
-      autoEscape = true;
-      autoUpdate = {
-        enable = true;
-        calendar = "daily";
-      };
-
-      networks.homelab = {
-        networkConfig = {
-          name = "homelab";
-          driver = "bridge";
-        };
-        serviceConfig.RemainAfterExit = true;
-      };
+      calendar = "daily";
     };
 
+    # The baseline bridge network for all homelab containers
+    networks.homelab = {
+      networkConfig = {
+        name = "homelab";
+        driver = "bridge";
+      };
+      serviceConfig.RemainAfterExit = true;
+    };
+  };
 }
-
