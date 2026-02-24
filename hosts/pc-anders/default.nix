94b7135bbf66d5b90fb3941a6c89aec5918a3def
Anders Munk <55032909+munkien@users.noreply.github.com>
2026-02-24 12:14:13 +0100

    Update default.nix

--- hosts/pc-anders/default.nix
+++ hosts/pc-anders/default.nix
@@ -6,4 +6,15 @@
 }: {
   # Naming
   networking.hostName = "pc-anders";
+
+  # Recovery shell
+  specialisation = {
+    "Recovery-Shell" = {
+      configuration = {
+        system.nixos.tags = ["recovery"];
+        services.getty.autologinUser = "root";
+        networking.hostName = lib.mkForce "nixos-recovery";
+      };
+    };
+  };
 }
