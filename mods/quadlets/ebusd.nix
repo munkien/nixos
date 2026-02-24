d0ccc88afffe9d684a25802d6713f601145471a9
Anders Munk <55032909+munkien@users.noreply.github.com>
2026-02-24 12:37:29 +0100

    Create ebusd.nix

---
+++ mods/quadlets/ebusd.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  common = import ./common-quadlet.nix { inherit lib; };
+  persistDir = "/persist/services/ebusd";
+in
+{
+  systemd.tmpfiles.rules = [
+    "d ${persistDir} 0755 root root -"
+  ];
+
+  virtualisation.quadlet.containers.ebusd = lib.recursiveUpdate common {
+    containerConfig = {
+      # Pinned image version to ensure declarative and reproducible builds
+      image = "docker.io/john30/ebusd:23.3"; 
+      
+      healthCmd = "ebusctl info";
+      networks = [ "host" ];
+
+      # Map the persistent directory so downloaded configs survive reboots
+      volumes = [
+        "${persistDir}:/var/lib/ebusd:rw"
+      ];
+
+      exec = [
+        "ebusd"
+        "-f"
+        "-d"
+        "ens:192.168.0.250:9999"
+        "--scanconfig"
+        "--mqttport=1883"
+        "--mqttjson"
+        "--mqtthost=127.0.0.1"
+      ];
+
+      # Retained volatile paths, removed /var/lib/ebusd
+      tmpfses = [
+        "/tmp"
+        "/var/run"
+        "/var/log"
+      ];
+    };
+  };
+}
