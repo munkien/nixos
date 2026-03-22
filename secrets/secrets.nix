f1beda79f7ae9dc044948ae710f4fa9d215ab249
munkien <munkien@gmail.com>
2026-03-22 21:08:56 +0100

    WIP

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   user_munkien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHBrIzRbUZF4n3SuvZHjzuFv+8vfQrS7Yvov+hjGWJ1";
   users = [user_munkien];
   system_anders_pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHX8xYUGCFSnNC2LfioaQUD1E4QVzLTAcAvlOo7dB110";
-  system_server_home = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9SkEsdW3iGc/FMDJvfFMmSE60JTbmoT6tGEmVMqfqf";
+  system_server_home = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBLOn8WFpEJa9SQG7BplBP3dZo00kXTJ2gaailooqeb";
   systems = [system_anders_pc system_server_home];
 in {
   "secret_wifi_env.age".publicKeys = users ++ systems;
