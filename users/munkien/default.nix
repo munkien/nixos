0b0477cd80312638a14275df7234aadb3aacb1e5
GL3 Homelab <homelab@mail.me>
2026-03-24 22:04:49 +0100

    fix

--- users/munkien/default.nix
+++ users/munkien/default.nix
@@ -9,5 +9,8 @@
     hashedPasswordFile = config.age.secrets."munkien_password_hashed".path;
     description = "Anders";
     extraGroups = ["networkmanager" "wheel"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHBrIzRbUZF4n3SuvZHjzuFv+8vfQrS7Yvov+hjGWJ1 munkien@gmail.com"
+    ];
   };
 }
