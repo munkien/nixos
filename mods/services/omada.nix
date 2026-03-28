84823d0131023e48eef892551c4cc2e37a43a5a7
munkien <munkien@gmail.com>
2026-03-28 22:51:56 +0100

    WIP

--- mods/services/omada.nix
+++ mods/services/omada.nix
@@ -5,17 +5,6 @@
   ...
 }: let
   common = import ./base-quadlet.nix {inherit lib config;};
-<<<<<<< HEAD
-in {
-  # 1. Base directories
-  systemd.tmpfiles.rules =
-    map
-    (d: "d /persist/services/omada${d} 0755 root root -")
-    ["" "/db" "/data" "/logs"];
-
-  # 2. Networking & Firewall
-  networking.firewall = {
-=======
   persistBase = "/persist/services/omada";
 in {
   systemd.tmpfiles.rules = map (args: "d ${persistBase}${args}") [
@@ -26,7 +15,6 @@ in {
 
   networking.firewall = {
     allowedTCPPorts = [80 443 8088 8043];
->>>>>>> 593ae6a (WIP)
     allowedTCPPortRanges = [
       {
         from = 29811;
@@ -45,48 +33,6 @@ in {
     '';
   };
 
-<<<<<<< HEAD
-  # 4. Containers
-  virtualisation.quadlet.containers = {
-    omada-db = lib.recursiveUpdate common {
-      serviceConfig = {
-        ExecStartPre = "+chown -R 100999:100999 /persist/services/omada/db";
-      };
-      containerConfig = {
-        image = "docker.io/library/mongo:8";
-        notify = "healthy";
-        healthCmd = "mongosh --port 27017 --eval 'db.adminCommand(\"ping\").ok' --quiet || exit 1";
-        publishPorts = ["127.0.0.1:27017:27017"];
-        tmpfses = ["/tmp" "/data/configdb"];
-        exec = "mongod --bind_ip_all --wiredTigerCacheSizeGB 0.5 --quiet";
-        volumes = ["/persist/services/omada/db:/data/db:rw,Z,U"];
-      };
-    };
-
-    omada = lib.recursiveUpdate common {
-      unitConfig = {
-        Requires = "omada-db.service";
-        After = "omada-db.service";
-      };
-      containerConfig = {
-        image = "docker.io/mbentley/omada-controller:6";
-        ulimits = ["nofile=4096:8192"];
-        networks = ["host"];
-        readOnly = false;
-        notify = "healthy";
-        healthStartPeriod = "5m";
-        healthCmd = "wget --quiet --tries=1 --no-check-certificate --spider http://127.0.0.1:8088/ || exit 1";
-        environments = {
-          ROOTLESS = "true";
-          MONGO_EXTERNAL = "true";
-          EAP_MONGOD_URI = "mongodb://127.0.0.1:27017/omada";
-        };
-        volumes = [
-          "/persist/services/omada/data:/opt/tplink/EAPController/data:rw,Z,U"
-          "/persist/services/omada/logs:/opt/tplink/EAPController/logs:rw,Z,U"
-        ];
-      };
-=======
   services.mongodb = {
     enable = true;
     package = pkgs.mongodb-6_0;
@@ -124,7 +70,6 @@ in {
         "${persistBase}/data:/opt/tplink/EAPController/data:rw,Z,U"
         "${persistBase}/logs:/opt/tplink/EAPController/logs:rw,Z,U"
       ];
->>>>>>> 593ae6a (WIP)
     };
   };
 }
