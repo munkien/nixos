6513ad00436afc26a8e602c4a26906a6da31e01a
munkien <munkien@gmail.com>
2026-04-04 16:38:16 +0200

    WIP

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,29 @@
 let
-  user_munkien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHBrIzRbUZF4n3SuvZHjzuFv+8vfQrS7Yvov+hjGWJ1";
-  users = [user_munkien];
-  system_anders_pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHX8xYUGCFSnNC2LfioaQUD1E4QVzLTAcAvlOo7dB110";
-  system_server_home = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBLOn8WFpEJa9SQG7BplBP3dZo00kXTJ2gaailooqeb";
-  systems = [system_anders_pc system_server_home];
+  key = path: builtins.readFile path;
+
+  # Users
+  users = {
+    munkien = key ../users/munkien/secret_key.pub;
+  };
+
+  # Systems
+  systems = {
+    pc-anders = key ../hosts/pc-anders/secret_key.pub;
+    # server-home-1 = key ../hosts/server-home-1/secret_key.pub;
+  };
+
+  allSystems = builtins.attrValues systems;
+  allUsers = builtins.attrValues users;
+
 in {
-  "common/wifi_env.age".publicKeys = users ++ systems;
-  "secret_munkien_password.age".publicKeys = [user_munkien] ++ systems;
-  "secret_service_password.age".publicKeys = [user_munkien] ++ systems;
-  "AUTHELIA_JWT_SECRET.age".publicKeys = [user_munkien system_server_home];
-  "AUTHELIA_SESSION_SECRET.age".publicKeys = [user_munkien system_server_home];
-  "AUTHELIA_STORAGE_ENCRYPTION_KEY.age".publicKeys = [user_munkien system_server_home];
-  "acme_env.age".publicKeys = [user_munkien system_server_home];
+  # Shared
+  "./common/wifi-gl3_env.age".publicKeys = allUsers ++ allSystems;
+
+  # Users
+  "../users/munkien/password.age".publicKeys = [ users.munkien ] ++ allSystems;
+
+  # Hosts
+
+  # Services
+  # "services/authelia/jwt.age".publicKeys = [ users.munkien systems.server-home ];
 }
