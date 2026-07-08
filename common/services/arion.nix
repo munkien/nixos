698c32c15b630e01806d0cd093d5a2c5789beee5
munkien <munkien@gmail.com>
2026-07-08 22:05:14 +0200

    WIP

--- common/services/arion.nix
+++ common/services/arion.nix
@@ -13,17 +13,5 @@
   virtualisation.arion.backend = "docker";
   virtualisation.arion.projects.home-infra.settings = {
     networks.internal_net = {};
-
-    services.watchtower.service = {
-      image = "nickfedor/watchtower:latest";
-      volumes = [
-        "/var/run/docker.sock:/var/run/docker.sock"
-      ];
-      environment = {
-        WATCHTOWER_CLEANUP = "true";
-        WATCHTOWER_POLL_INTERVAL = "86400";
-      };
-      restart = "always";
-    };
   };
 }
