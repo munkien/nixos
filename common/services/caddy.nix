2971a632bfa76b9e9165bc7204d91756fa5b3471
munkien <munkien@gmail.com>
2026-07-08 23:29:05 +0200

    WIP

--- common/services/caddy.nix
+++ common/services/caddy.nix
@@ -20,6 +20,6 @@
 
   services.caddy.package = pkgs.caddy.withPlugins {
     plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-    hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
+    hash = "sha256-pB7YlSeVXyYahCDYKDmEtY39Wtr0kKWd1w2Bs2qMEfY=";
   };
 }
