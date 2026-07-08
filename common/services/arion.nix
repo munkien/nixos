438434a63379fde7100ca3a2ce0e8a5b35d483c5
munkien <munkien@gmail.com>
2026-07-08 23:11:09 +0200

    WIP

--- common/services/arion.nix
+++ common/services/arion.nix
@@ -10,7 +10,7 @@
 
   users.extraUsers.munkien.extraGroups = ["podman" "docker"];
 
-  virtualisation.arion.backend = "podman";
+  virtualisation.arion.backend = "podman-socket";
   virtualisation.arion.projects.home-infra.settings = {
     networks.internal_net = {};
   };
