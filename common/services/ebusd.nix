4be52bc837d9fd2d66b1187619e7b8dfaedd8df9
munkien <munkien@gmail.com>
2026-07-17 22:19:55 +0200

    WIP

---
+++ common/services/ebusd.nix
@@ -0,0 +1,17 @@
+{...}: {
+  services.ebusd = {
+    enable = true;
+    device = "ens:192.168.88.248:9999";
+    mqtt = {
+      enable = true;
+      host = "localhost";
+      port = 1883;
+      home-assistant = true;
+    };
+    extraOptions = [
+      "--mqttjson"
+      "--accesslevel=*"
+      "--pollinterval=30"
+    ];
+  };
+}
