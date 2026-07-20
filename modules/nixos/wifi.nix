6577f7c9a6dd69bd70478c4a7e3faa92d6421cf2
munkien <munkien@gmail.com>
2026-07-20 19:17:21 +0200

    WIP

--- modules/nixos/wifi.nix
+++ modules/nixos/wifi.nix
@@ -1,13 +1,12 @@
 _: {
-  boot.extraModprobeConfig = ''
-    options iwlmvm power_scheme=1
-  '';
-
   networking = {
     wireless.enable = false;
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
+      wifi = {
+        backend = "iwd";
+        powersave = false;
+      };
     };
   };
 }
