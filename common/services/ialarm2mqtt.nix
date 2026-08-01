532ca786ff448ac6aa0e96a1d267f50ac9b2ebc0
munkien <munkien@gmail.com>
2026-08-01 18:02:28 +0200

    WIP

--- common/services/ialarm2mqtt.nix
+++ common/services/ialarm2mqtt.nix
@@ -3,20 +3,18 @@
   pkgs,
   ...
 }: {
-  # This assumes you have encrypted your complete config.yaml into this .age file
   age.secrets.ialarm-config = {
     rekeyFile = ../../secrets/services/ialarm2mqtt.age;
     symlink = false;
     mode = "0444";
   };
 
-  # 2. Bind-mount the decrypted path into your Arion project
   virtualisation.arion.projects.home-infra.settings.services.ialarm2mqtt.service = {
     image = "docker.io/maxill1/ialarm-mqtt:v0.12.0";
     container_name = "ialarm2mqtt";
 
     volumes = [
-      "${config.age.secrets.ialarm-config.path}:/config/config.yaml:ro"
+      "${config.age.secrets.ialarm-config.path}:/config/config.json:ro"
     ];
 
     restart = "always";
